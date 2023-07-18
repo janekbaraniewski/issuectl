9f0453d8113094a96a087580c1f4ca9bccd6d534
Jan Baraniewski <dev@baraniewski.com>
2023-07-18 21:24:52 +0200

    Fix interfaces

--- pkg/backend_github.go
+++ pkg/backend_github.go
@@ -12,10 +12,12 @@ import (
 )
 
 type GitHub struct {
-	client *github.Client
+	baseURL string
+	token   string
+	client  *github.Client
 }
 
-func NewGitHubClient(token string) *GitHub {
+func NewGitHubClient(token, baseURL string) *GitHub {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
@@ -23,18 +25,18 @@ func NewGitHubClient(token string) *GitHub {
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
-	return &GitHub{client: client}
+	return &GitHub{client: client, baseURL: baseURL, token: token}
 }
 
 // IssueExists is DEPRECATED. use GetIssue instead
-func (g *GitHub) IssueExists(owner, repo string, issueID IssueID) (bool, error) {
+func (g *GitHub) IssueExists(owner string, repo RepoConfigName, issueID IssueID) (bool, error) {
 	// TODO: deprecate, use GetIssue instead
 	issueNumber, err := getIssueNumberFromString(issueID)
 	if err != nil {
 		return false, err
 	}
 
-	_, _, err = g.client.Issues.Get(context.Background(), owner, repo, issueNumber)
+	_, _, err = g.client.Issues.Get(context.Background(), owner, string(repo), issueNumber)
 	if err != nil {
 		return false, err
 	}
@@ -56,14 +58,14 @@ func (g *GitHub) GetIssue(owner, repo string, issueID IssueID) (*github.Issue, e
 	return issue, nil
 }
 
-func (g *GitHub) CloseIssue(owner, repo string, issueID IssueID) error {
+func (g *GitHub) CloseIssue(owner string, repo RepoConfigName, issueID IssueID) error {
 	issueNumber, err := getIssueNumberFromString(issueID)
 	if err != nil {
 		return err
 	}
 
 	issueRequest := &github.IssueRequest{State: github.String("closed")}
-	_, _, err = g.client.Issues.Edit(context.Background(), owner, repo, issueNumber, issueRequest)
+	_, _, err = g.client.Issues.Edit(context.Background(), owner, string(repo), issueNumber, issueRequest)
 	if err != nil {
 		return err
 	}
@@ -88,8 +90,8 @@ func (g *GitHub) OpenPullRequest(owner, repo, title, body, baseBranch, headBranc
 	return nil
 }
 
-func (g *GitHub) LinkIssueToRepo(baseURL, owner, repo, issueID, pullRequestID, token string) error {
-	url := fmt.Sprintf("%s/repos/%s/%s/issues/%s/timeline", baseURL, owner, repo, issueID)
+func (g *GitHub) LinkIssueToRepo(owner string, repo RepoConfigName, issueID IssueID, pullRequestID string) error {
+	url := fmt.Sprintf("%s/repos/%s/%s/issues/%s/timeline", g.baseURL, owner, repo, issueID)
 	body := map[string]string{
 		"issue_number": pullRequestID,
 	}
@@ -102,7 +104,7 @@ func (g *GitHub) LinkIssueToRepo(baseURL, owner, repo, issueID, pullRequestID, t
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Authorization", "Bearer "+g.token)
 	req.Header.Set("Accept", "application/vnd.github.starfire-preview+json")
 
 	resp, err := http.DefaultClient.Do(req)
