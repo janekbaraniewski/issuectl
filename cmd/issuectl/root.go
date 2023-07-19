ed1da45ee30316bcdafcf9adc1b14ca19869f486
Jan Baraniewski <dev@baraniewski.com>
2023-07-19 23:04:09 +0200

    Update cli description

--- cmd/issuectl/root.go
+++ cmd/issuectl/root.go
@@ -7,14 +7,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	ShortDescription string = "issuectl helps you manage separate environments for work on multiple issues"
+	LongDescription  string = `
+issuectl helps managing separate environments for work on multiple issues.
+
+Start work on issue:
+	issuectl start [issue_number]
+
+Open PR and link it to issue
+	issuectl openpr [issue_number]
+
+Finish work, close the issue
+	issuectl finish [issue_number]
+`
+)
+
 func RootCmd(version string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "issuectl",
 		Version: version,
-		Short:   "issuectl",
-		Long: `issuectl
-	issuectl
-		issuectl`,
+		Short:   ShortDescription,
+		Long:    LongDescription,
 		Run: func(cmd *cobra.Command, args []string) {
 
 		},
