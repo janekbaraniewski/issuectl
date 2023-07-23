9b505f1b17a359fe950009baa40fe7855b360606
Jan Baraniewski <dev@baraniewski.com>
2023-07-23 16:24:39 +0200

    start issue in jira

--- pkg/backend_jira.go
+++ pkg/backend_jira.go
@@ -43,11 +43,11 @@ func (j *Jira) GetIssue(owner string, repo RepoConfigName, issueID IssueID) (int
 }
 
 func (j *Jira) StartIssue(owner string, repo RepoConfigName, issueID IssueID) error {
-	return j.moveIssueToState(issueID, InProgress)
+	return j.moveIssueToState(issueID, InProgress, "On it 👀")
 }
 
 func (j *Jira) CloseIssue(owner string, repo RepoConfigName, issueID IssueID) error {
-	return j.moveIssueToState(issueID, Done)
+	return j.moveIssueToState(issueID, Done, "✅")
 }
 
 func (j *Jira) LinkIssueToRepo(owner string, repo RepoConfigName, issueID IssueID, pullRequestID string) error {
@@ -64,7 +64,7 @@ func (j *Jira) LinkIssueToRepo(owner string, repo RepoConfigName, issueID IssueI
 	return nil
 }
 
-func (j *Jira) moveIssueToState(issueID IssueID, desiredState string) error {
+func (j *Jira) moveIssueToState(issueID IssueID, desiredState string, message string) error {
 	issue, _, err := j.client.Issue.Get(string(issueID), nil)
 	if err != nil {
 		return err
@@ -96,6 +96,16 @@ func (j *Jira) moveIssueToState(issueID IssueID, desiredState string) error {
 	_, err = j.client.Issue.DoTransition(string(issueID), transitionID)
 	if err != nil {
 		return err
+	}
+
+	if message != "" {
+		comment := jira.Comment{
+			Body: message,
+		}
+		_, _, err = j.client.Issue.AddComment(string(issueID), &comment)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
