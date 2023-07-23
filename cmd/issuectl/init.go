bd12d5d87400389fb64029768812fbe697cf63f7
Jan Baraniewski <dev@baraniewski.com>
2023-07-23 17:49:31 +0200

    Fix init command after changes to backends

--- cmd/issuectl/init.go
+++ cmd/issuectl/init.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/AlecAivazis/survey/v2"
 	issuectl "github.com/janekbaraniewski/issuectl/pkg"
@@ -57,7 +58,7 @@ func askForBackend() (issuectl.BackendConfig, error) {
 			Name: "Type",
 			Prompt: &survey.Select{
 				Message: "Select backend type:",
-				Options: []string{string(issuectl.BackendGithub), string(issuectl.BackendGitLab)},
+				Options: []string{string(issuectl.BackendGithub), string(issuectl.BackendGitLab), string(issuectl.BackendJira)},
 			},
 			Validate: survey.Required,
 		},
@@ -65,10 +66,154 @@ func askForBackend() (issuectl.BackendConfig, error) {
 	if err := survey.Ask(prompt, &answers); err != nil {
 		return issuectl.BackendConfig{}, err
 	}
-	return issuectl.BackendConfig{
-		Name: issuectl.BackendConfigName("default"),
-		Type: issuectl.BackendType(answers.Type),
-		// Token: base64.RawStdEncoding.EncodeToString([]byte(answers.Token)),
+	switch answers.Type {
+
+	case string(issuectl.BackendGithub):
+		ghConf, err := askForGitHubConfig()
+		if err != nil {
+			return issuectl.BackendConfig{}, err
+		}
+		return issuectl.BackendConfig{
+			Name:   issuectl.BackendConfigName("default"),
+			Type:   issuectl.BackendType(answers.Type),
+			GitHub: &ghConf,
+		}, nil
+
+	case string(issuectl.BackendGitLab):
+		glConf, err := askForGitLabConfig()
+		if err != nil {
+			return issuectl.BackendConfig{}, err
+		}
+		return issuectl.BackendConfig{
+			Name:   issuectl.BackendConfigName("default"),
+			Type:   issuectl.BackendType(answers.Type),
+			GitLab: &glConf,
+		}, nil
+
+	case string(issuectl.BackendJira):
+		jiraConf, err := askForJiraConfig()
+		if err != nil {
+			return issuectl.BackendConfig{}, err
+		}
+		return issuectl.BackendConfig{
+			Name: issuectl.BackendConfigName("default"),
+			Type: issuectl.BackendType(answers.Type),
+			Jira: &jiraConf,
+		}, nil
+
+	default:
+		return issuectl.BackendConfig{
+			Name: issuectl.BackendConfigName("default"),
+			Type: issuectl.BackendType(answers.Type),
+			// Token: base64.RawStdEncoding.EncodeToString([]byte(answers.Token)),
+		}, nil
+
+	}
+}
+
+func askForGitHubConfig() (issuectl.GitHubConfig, error) {
+	answers := struct {
+		Host     string
+		Token    string
+		Username string
+	}{}
+	prompt := []*survey.Question{
+		{
+			Name:   "Host",
+			Prompt: &survey.Input{Message: "Enter GitHub Host (Skip for https://api.github.com/):"},
+		},
+		{
+			Name:     "Token",
+			Prompt:   &survey.Password{Message: "Enter GitHub Token:"},
+			Validate: survey.Required,
+		},
+		{
+			Name:     "Username",
+			Prompt:   &survey.Input{Message: "Enter GitHub Username:"},
+			Validate: survey.Required,
+		},
+	}
+	if err := survey.Ask(prompt, &answers); err != nil {
+		return issuectl.GitHubConfig{}, err
+	}
+	host := answers.Host
+	if host == "" {
+		host = "https://api.github.com/"
+	}
+	return issuectl.GitHubConfig{
+		Host:     answers.Host,
+		Token:    answers.Token,
+		Username: answers.Username,
+	}, nil
+}
+
+func askForGitLabConfig() (issuectl.GitLabConfig, error) {
+	answers := struct {
+		Host   string
+		Token  string
+		UserID string
+	}{}
+	prompt := []*survey.Question{
+		{
+			Name:     "Host",
+			Prompt:   &survey.Input{Message: "Enter GitLab Host:"},
+			Validate: survey.Required,
+		},
+		{
+			Name:     "Token",
+			Prompt:   &survey.Password{Message: "Enter GitLab Token:"},
+			Validate: survey.Required,
+		},
+		{
+			Name:     "UserID",
+			Prompt:   &survey.Input{Message: "Enter GitLab UserID:"},
+			Validate: survey.Required,
+		},
+	}
+	if err := survey.Ask(prompt, &answers); err != nil {
+		return issuectl.GitLabConfig{}, err
+	}
+	userID, err := strconv.Atoi(answers.UserID)
+	if err != nil {
+		return issuectl.GitLabConfig{}, err
+	}
+	return issuectl.GitLabConfig{
+		Host:   answers.Host,
+		Token:  answers.Token,
+		UserID: userID,
+	}, nil
+}
+
+func askForJiraConfig() (issuectl.JiraConfig, error) {
+	answers := struct {
+		Host     string
+		Token    string
+		Username string
+	}{}
+	prompt := []*survey.Question{
+		{
+			Name:     "Host",
+			Prompt:   &survey.Input{Message: "Enter Jira Host:"},
+			Validate: survey.Required,
+		},
+		{
+			Name:     "Token",
+			Prompt:   &survey.Password{Message: "Enter Jira Token:"},
+			Validate: survey.Required,
+		},
+		{
+			Name:     "Username",
+			Prompt:   &survey.Input{Message: "Enter Jira Username:"},
+			Validate: survey.Required,
+		},
+	}
+	if err := survey.Ask(prompt, &answers); err != nil {
+		return issuectl.JiraConfig{}, err
+	}
+	return issuectl.JiraConfig{
+		Host:     answers.Host,
+		Token:    answers.Token,
+		Username: answers.Username,
 	}, nil
 }
 
