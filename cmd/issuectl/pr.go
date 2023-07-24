08e67483892fa168252e017260f7e2e03a3ac39b
Jan Baraniewski <dev@baraniewski.com>
2023-07-24 17:52:16 +0200

    Update custom PR name

--- cmd/issuectl/pr.go
+++ cmd/issuectl/pr.go
@@ -42,22 +42,26 @@ func getIssueIDFromParentDirectory(config issuectl.IssuectlConfig) string {
 
 func initOpenPullRequestCommand(rootCmd *cobra.Command) {
 	openPRCmd := &cobra.Command{
-		Use:   "openpr [issue number]",
-		Short: "Opens a pull request for the specified issue",
-		Long:  `This command opens a pull request for the specified issue on GitHub`, // FIXME: Github??                                                 // it requires exactly one argument
+		Use:   "openpr [issue number] [pr title]",
+		Short: "Opens a pull request for the specified issue. You can specify title, if left empty default title will be generated from issue title",
+		Long:  `This command opens a pull request for the specified issue in Repository Backend`, // FIXME: Github??                                                 // it requires exactly one argument
 		RunE: func(cmd *cobra.Command, args []string) error {
 			config := issuectl.LoadConfig()
 			var issueID string
+			var customTitle string
 			if len(args) == 1 {
 				issueID = args[0]
 			} else if len(args) == 0 {
 				// check if can detect issue from pwd
 				issueID = getIssueIDFromParentDirectory(config)
+			} else if len(args) == 2 {
+				issueID = args[0]
+				customTitle = args[1]
 			}
 			if issueID == "" {
 				return errors.New("Missing issueID")
 			}
-			err := issuectl.OpenPullRequest(issuectl.IssueID(issueID))
+			err := issuectl.OpenPullRequest(issuectl.IssueID(issueID), customTitle)
 			if err != nil {
 				return err
 			}
