ffd36a303095bf1a31d1b6e5eda8d57e932f709f
Jan Baraniewski <dev@baraniewski.com>
2023-08-11 01:16:16 +0200

    Add public docs

---
+++ publicWebsite/src/views/home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home-container">
+    <h1 class="home-text">issuectl</h1>
+    <span class="home-text1">
+
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  metaInfo: {
+    title: 'Beneficial Funny Skunk',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Beneficial Funny Skunk',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.home-text {
+  font-size: 60px;
+  font-style: normal;
+  font-weight: 100;
+}
+.home-text1 {
+  font-style: normal;
+  font-family: Roboto;
+  font-weight: 100;
+}
+</style>
